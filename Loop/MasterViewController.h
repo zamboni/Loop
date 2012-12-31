f48a19705acbb8fc067359e010afda593c1c4825
Fletcher Fowler <fletch@zambonidev.com>
2012-12-31 14:18:46 -0700

    Initial Commit

---
+++ Loop/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Loop
+//
+//  Created by Fletcher Fowler on 12/31/12.
+//  Copyright (c) 2012 ZamboniDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
---
+++ LoopTests/LoopTests.m
@@ -0,0 +1,32 @@
+//
+//  LoopTests.m
+//  LoopTests
+//
+//  Created by Fletcher Fowler on 12/31/12.
+//  Copyright (c) 2012 ZamboniDev. All rights reserved.
+//
+
+#import "LoopTests.h"
+
+@implementation LoopTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LoopTests");
+}
+
+@end
