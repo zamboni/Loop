5e0b7fdb749a16be852be8b408e689f52fd986e0
Fletcher Fowler <fletch@zambonidev.com>
2012-12-31 15:04:48 -0700

    T: added Kiwi

--- LoopTests/LoopTests.m
+++ LoopTests/LoopTests.m
@@ -1,32 +1,12 @@
-//
-//  LoopTests.m
-//  LoopTests
-//
-//  Created by Fletcher Fowler on 12/31/12.
-//  Copyright (c) 2012 ZamboniDev. All rights reserved.
-//
+#import "Kiwi.h"
 
-#import "LoopTests.h"
+SPEC_BEGIN(SpecName)
 
-@implementation LoopTests
+describe(@"TestClass", ^{
+    it(@"should have tests", ^{
+        NSString *string = @"yada";
+        [string shouldNotBeNil];
+    });
+});
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in LoopTests");
-}
-
-@end
+SPEC_END
---
+++ Pods/PodsDummy_Pods_LoopTests.m
@@ -0,0 +1,4 @@
+@interface PodsDummy_Pods_LoopTests : NSObject
+@end
+@implementation PodsDummy_Pods_LoopTests
+@end
