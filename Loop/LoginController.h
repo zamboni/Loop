dc040191c46e8711aa7d3140a2898c198d9f56d4
Fletcher Fowler <fletch@zambonidev.com>
2013-01-11 15:53:21 -0700

    T: adding login controller

---
+++ Loop/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  LoginController.h
+//  Loop
+//
+//  Created by Fletcher Fowler on 1/11/13.
+//  Copyright (c) 2013 ZamboniDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginController : UINavigationController <UITextFieldDelegate>
+
+@end
---
+++ Loop/LoginController.m
@@ -0,0 +1,38 @@
+//
+//  LoginController.m
+//  Loop
+//
+//  Created by Fletcher Fowler on 1/11/13.
+//  Copyright (c) 2013 ZamboniDev. All rights reserved.
+//
+
+#import "LoginController.h"
+
+@interface LoginController ()
+
+@end
+
+@implementation LoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
